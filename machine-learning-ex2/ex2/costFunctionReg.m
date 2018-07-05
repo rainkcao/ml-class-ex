91e6a2ed365d570afb98633f44c6520bcedf0858
Cao Yukun <caoyukun@chasingtiems.com>
2018-07-05 20:17:57 +0800

    添加第四周第一题代码

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 
 
 h = sigmoid(X*theta);
-J = (1/m)*sum(-y.*log(h)-(1-y).*log(1-h)) + (lambda/(2*m))*sum(theta(2:size(theta,1),1).^2);
+J = (1/m)*sum(-y.*log(h)-(1-y).*log(1-h)) + (lambda/(2*m))*sum(theta.^2);
 grad(1,1) = (1/m)*sum((h-y).*X(:,1));
 for j = 2:size(theta,1)
 	grad(j,1) = (1/m)*sum((h-y).*X(:,j)) + (lambda/m)*theta(j,1);
--- machine-learning-ex2/ex2/ex2.m
+++ machine-learning-ex2/ex2/ex2.m
@@ -94,7 +94,7 @@ pause;
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 
 %  Run fminunc to obtain the optimal theta
-%  This function will return theta and the cost 
+%  This function will return theta and the cost
 [theta, cost] = ...
 	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
 
